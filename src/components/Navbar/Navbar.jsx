049024d3ad2c1e30f0fcba6ae84297bf60280755
Aiswarya H <aiswarya12hari@gmail.com>
2026-08-04 11:20:10 +0530

    seven

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       {/* Change this to your actual names or initials. Adding an & and dot makes it elegant */}
-      <div className="nav-logo">S <span className="ampersand">&amp;</span> K</div>
+      <div className="nav-logo">K <span className="ampersand">&amp;</span> S</div>
       <div className="nav-links">
         <a href="#countdown">When</a>
         <a href="#venue">Where</a>
